685c606f03c58960f5cff9e6fad18815c08ddd3b
Camron Schwoegler <camron@metafour.org>
2013-07-31 00:31:35 -0600

    Finish Chapter 5 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,1 @@
-def full_title(page_title)
-  if page_title.empty?
-    "Ruby on Rails Tutorial Sample App"
-  else
-    "Ruby on Rails Tutorial Sample App | #{page_title}"
-  end
-end
+include ApplicationHelper
