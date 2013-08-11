b9d05d140174c004a64de6ffe7abae906b64397d
Camron Schwoegler <camron@metafour.org>
2013-08-10 21:54:05 -0600

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { email.downcase! }
+  before_create :create_remember_token
   validates :name, presence: true, length: 3..50 # { maximum: 50, minimum: 3 }
   # Allows foo@bar..com
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +9,19 @@ class User < ActiveRecord::Base
              format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ SampleApp::Application.routes.draw do
 #  get "static_pages/contact"
 
 resources :users
+resources :sessions, only: [:new, :create, :destroy]
 
 root 'static_pages#home'
 
 match '/signup', to: 'users#new', via: 'get'
+match '/signin', to: 'sessions#new', via: 'get'
+match '/signout', to: 'sessions#destroy', via: 'delete'
 
 match '/help', to: 'static_pages#help', via: 'get'
 #get '/help' => 'static_pages#help'
